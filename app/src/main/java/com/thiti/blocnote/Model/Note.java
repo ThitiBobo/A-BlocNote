13b748bd0a70b7cb1dc03acf567b6b5f1eb1c771
Thibaut Delplanque <t.delplanque92@gmail.com>
2018-02-23 15:47:37 +0100

    ajout des fonction onCreate et onUpgrade dans DataBaseOpenHelper

--- app/src/main/java/com/thiti/blocnote/Model/Note.java
+++ app/src/main/java/com/thiti/blocnote/Model/Note.java
@@ -1,10 +1,11 @@
-package com.thiti.blocnote;
+package com.thiti.blocnote.Model;
 
 /**
  * Created by canard on 23/02/18.
  */
 
 public class Note {
+
 
     private String mTitle;
     private String mContent;
@@ -31,6 +32,6 @@ public class Note {
     }
 
     public Note(String title) {
-        this(title,"il y a rien dans ta note PD !!!");
+        this(title, new String());
     }
 }
--- app/src/main/java/com/thiti/blocnote/NoteViewHolder.java
+++ app/src/main/java/com/thiti/blocnote/NoteViewHolder.java
@@ -5,6 +5,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
 
+import com.thiti.blocnote.Model.Note;
+
 
 public class NoteViewHolder extends RecyclerView.ViewHolder {
 
